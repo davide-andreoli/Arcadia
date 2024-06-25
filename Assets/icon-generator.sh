20d7881ac17a2544b109e7ddcc618f0e50f27256
Davide Andreoli <davide.andreoli@icloud.com>
2024-06-25 20:47:59 +0200

    script

--- Assets/icon-generator.sh
+++ Assets/icon-generator.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Check if filename is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
+
 # Define the filename to search for
-FILENAME="Arcadia.svg"
+FILENAME="$1"
+
+# Extract the base name without extension
+BASENAME=$(basename "$FILENAME" .svg)
 
 # Find the file in the current directory
 FILEPATH=$(find . -name "$FILENAME" -print -quit)
@@ -21,95 +30,36 @@ if [[ ! -x "$INKSCAPE" ]]; then
   exit 1
 fi
 
-# Define the desired export width
-EXPORT_WIDTH=800
+# Define the desired export widths and filenames
+declare -A EXPORTS=(
+  [1024]="$BASENAME-macOS-512pt-icon@2x.png $BASENAME-iOS-1024pt-icon.png"
+  [512]="$BASENAME-macOS-512pt-icon.png $BASENAME-macOS-256pt-icon@2x.png"
+  [256]="$BASENAME-macOS-256pt-icon.png $BASENAME-macOS-128pt-icon@2x.png"
+  [192]="$BASENAME-iOS-64pt-icon@3x.png"
+  [180]="$BASENAME-iOS-60pt-icon@3x.png"
+  [167]="$BASENAME-iOS-83-5pt-icon@2x.png"
+  [152]="$BASENAME-iOS-76pt-icon@2x.png"
+  [136]="$BASENAME-iOS-68pt-icon@2x.png"
+  [128]="$BASENAME-macOS-128pt-icon.png $BASENAME-macOS-64pt-icon@2x.png $BASENAME-iOS-64pt-icon@2x.png"
+  [120]="$BASENAME-iOS-60pt-icon@2x.png $BASENAME-iOS-40pt-icon@3x.png"
+  [114]="$BASENAME-iOS-38pt-icon@3x.png"
+  [87]="$BASENAME-iOS-29pt-icon@3x.png"
+  [80]="$BASENAME-iOS-40pt-icon@2x.png"
+  [76]="$BASENAME-iOS-38pt-icon@2x.png"
+  [64]="$BASENAME-macOS-64pt-icon.png $BASENAME-macOS-32pt-icon@2x.png"
+  [60]="$BASENAME-iOS-20pt-icon@3x.png"
+  [58]="$BASENAME-iOS-29pt-icon@2x.png"
+  [40]="$BASENAME-iOS-20pt-icon@2x.png"
+  [32]="$BASENAME-macOS-32pt-icon.png $BASENAME-macOS-16pt-icon@2x.png"
+  [16]="$BASENAME-macOS-16pt-icon.png"
+)
 
-# 1024
-EXPORT_WIDTH=1024
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-512pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-1024pt-icon.png" --export-width="$EXPORT_WIDTH"
-
-# 512
-EXPORT_WIDTH=512
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-512pt-icon.png" --export-width="$EXPORT_WIDTH"
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-256pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 256
-EXPORT_WIDTH=256
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-256pt-icon.png" --export-width="$EXPORT_WIDTH"
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-128pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 192
-EXPORT_WIDTH=192
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-64pt-icon@3x.png" --export-width="$EXPORT_WIDTH"
-
-# 180
-EXPORT_WIDTH=180
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-60pt-icon@3x.png" --export-width="$EXPORT_WIDTH"
-
-# 167
-EXPORT_WIDTH=167
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-83-5pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 152
-EXPORT_WIDTH=152
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-76pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 136
-EXPORT_WIDTH=136
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-68pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 128
-EXPORT_WIDTH=128
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-128pt-icon.png" --export-width="$EXPORT_WIDTH"
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-64pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-64pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 114
-EXPORT_WIDTH=114
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-38pt-icon@3x.png" --export-width="$EXPORT_WIDTH"
-
-# 120
-EXPORT_WIDTH=120
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-60pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-40pt-icon@3x.png" --export-width="$EXPORT_WIDTH"
-
-# 87
-EXPORT_WIDTH=87
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-29pt-icon@3x.png" --export-width="$EXPORT_WIDTH"
-
-# 80
-EXPORT_WIDTH=80
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-40pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 76
-EXPORT_WIDTH=76
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-38pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 64
-EXPORT_WIDTH=64
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-64pt-icon.png" --export-width="$EXPORT_WIDTH"
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-32pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 60
-EXPORT_WIDTH=60
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-20pt-icon@3x.png" --export-width="$EXPORT_WIDTH"
-
-# 58
-EXPORT_WIDTH=58
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-29pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 40
-EXPORT_WIDTH=40
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-iOS-20pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 32
-EXPORT_WIDTH=32
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-32pt-icon.png" --export-width="$EXPORT_WIDTH"
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-16pt-icon@2x.png" --export-width="$EXPORT_WIDTH"
-
-# 16
-EXPORT_WIDTH=16
-"$INKSCAPE" "$FILEPATH" --export-filename="Arcadia-macOS-16pt-icon.png" --export-width="$EXPORT_WIDTH"
+# Loop through the export widths and filenames
+for WIDTH in "${!EXPORTS[@]}"; do
+  FILENAMES="${EXPORTS[$WIDTH]}"
+  for FILENAME_OUT in $FILENAMES; do
+    "$INKSCAPE" "$FILEPATH" --export-filename="$FILENAME_OUT" --export-width="$WIDTH"
+  done
+done
 
 echo "Inkscape processing completed."
